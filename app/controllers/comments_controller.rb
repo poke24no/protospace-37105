09a7926d14671f22d9a2dd53551918359bdd128a
poke24no <mopn@icloud.com>
2021-11-03 14:18:23 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+
+  def create
+
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = @comment.prototype
+      render "prototypes/show"
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
+
+# 1, proto --- comment 1
+# 2, proto --- comment 2
+# 3,
+# 4,
+
+# プロトに紐付いているユーザーprototype.user
